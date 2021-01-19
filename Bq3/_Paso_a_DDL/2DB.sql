d6a62d9df414c2eddd774db16b815440ffedfae8
David <david.cesar.aliseda@gmail.com>
2021-01-19 17:15:59 +0100

    19/1/2021

--- Bq3/_Paso_a_DDL/2DB.sql
+++ Bq3/_Paso_a_DDL/2DB.sql
@@ -9,6 +9,13 @@ CREATE TABLE proyecto
 	nombre VARCHAR(50)
 );
 
+INSERT INTO proyecto (codigo, nombre )
+VALUES ('135','Skyfall');
+INSERT INTO proyecto (codigo, nombre )
+VALUES ('52','Spectre');
+INSERT INTO proyecto (codigo, nombre )
+VALUES ('2685','Starlight');
+
 CREATE TABLE empleado
 (
 	dni VARCHAR(9) PRIMARY KEY,
@@ -20,10 +27,23 @@ CREATE TABLE empleado
 
 );
 
+INSERT INTO empleado (dni, nombre, sueldo, fecha_asig, codigo)
+VALUES ('12345678C''Federico','1320', '11-6-2020', '135');
+INSERT INTO empleado (dni, nombre, sueldo, fecha_asig, codigo)
+VALUES ('65482154C''Antonio','1450', '11-13-2020', '135');
+INSERT INTO empleado (dni, nombre, sueldo, fecha_asig, codigo)
+VALUES ('67892451C''Sara','1675', '2-1-2021', '2685');
+INSERT INTO empleado (dni, nombre, sueldo, fecha_asig, codigo)
+VALUES ('35489652C''Eugenio','1320', '7-8-2020', '135');
+
 CREATE TABLE jefe
 (
 	dni VARCHAR(9) PRIMARY KEY,
 	dni_jefe VARCHAR(9),
 	CONSTRAINT fk_emple_jefe FOREIGN KEY (dni) REFERENCES empleado (dni) ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+INSERT INTO jefe (dni, dni_jefe)
+VALUES ('35489652C''Eugenio','1320', '7-8-2020', '135');
+
 
